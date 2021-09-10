b550c1d6d5f8d5ec66b1d2d09debb471c1a3db88
somariosidharta <49771935+somariosidharta@users.noreply.github.com>
2021-09-10 16:50:29 +0700

    simplify node checking

--- main_test.go
+++ main_test.go
@@ -50,14 +50,14 @@ func Test_evaluateSignal(t *testing.T) {
 		{
 			name: "good signal",
 			args: args{
-				signal: []int{0, 1, 1, 0, 0, 1},
+				signal: []int{1, 1, 1, 0, 0, 1},
 			},
 			want: "good",
 		},
 		{
 			name: "bad signal",
 			args: args{
-				signal: []int{1, 0, 1, 1, 0, 0},
+				signal: []int{0, 0, 1, 1, 0, 0},
 			},
 			want: "bad",
 		},
@@ -66,42 +66,6 @@ func Test_evaluateSignal(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := evaluateSignal(tt.args.signal); got != tt.want {
 				t.Errorf("calculateSignal() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_evaluateNode(t *testing.T) {
-	type args struct {
-		node1 int
-		node2 int
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{
-			name: "test1",
-			args: args{
-				node1: 1,
-				node2: 0,
-			},
-			want: 0,
-		},
-		{
-			name: "test2",
-			args: args{
-				node1: 0,
-				node2: 1,
-			},
-			want: 1,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := evaluateNode(tt.args.node1, tt.args.node2); got != tt.want {
-				t.Errorf("evaluateNode() = %v, want %v", got, tt.want)
 			}
 		})
 	}
